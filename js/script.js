9fcea8b33f4600058f6f4ae0a08f3b9da1ab1fb0
yuriy <ymarkovka@gmail.com>
2017-09-06 23:32:22 +0300

    Оживление формы.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var search = document.querySelector(".search-button");
+var modalForm = document.querySelector(".modal-form");
+var focus = document.querySelector("[name=arrivel-time]");
+var departureDate =document.querySelector("[name=departure-time]");
+var form = document.querySelector("form");
+
+search.addEventListener("click", function(evt){
+  
+  if(!modalForm.classList.contains("modal-form-show")){  
+    evt.preventDefault();
+    modalForm.classList.add("modal-form-show");
+    focus.focus();
+  }
+  else if(modalForm.classList.contains("modal-form-show")){
+    evt.preventDefault();
+    modalForm.classList.remove("modal-form-show");
+    modalForm.classList.remove("modal-form-error");
+  }
+});
+
+form.addEventListener("submit", function(evt){
+    if(!focus.value || !departureDate.value){
+      evt.preventDefault();
+      modalForm.classList.remove("modal-form-error");
+      modalForm.offsetWidth = modalForm.offsetWidth;
+      modalForm.classList.add("modal-form-error");
+    }
+});
+window.addEventListener("keydown", function(evt){
+    if(evt.keyCode === 27){
+      modalForm.classList.remove("modal-form-show");
+      modalForm.classList.remove("modal-form-error");
+    }
+});
